f5f8f0c57778dfbecbecdd18ef08d5c7fad0f538
sothulthorn <thornsothul@gmail.com>
2025-01-17 21:45:12 +0900

    Building navbar, footer and about page and allow html content

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('post/<slug:slug>', views.post_page, name='post_page'),
     path('tag/<slug:slug>', views.tag_page, name='tag_page'),
     path('author/<slug:slug>', views.author_page, name='author_page'),
-    path('search/', views.search_posts, name='search')
+    path('search/', views.search_posts, name='search'),
+    path('about/', views.about, name='about'),
 ]
